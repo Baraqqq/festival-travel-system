342c791e478ec81e65145f0b59aa7ae2f14be8b2
Baraqqq <iliasbach@hotmail.com>
2025-04-04 22:54:07 +0200

    Points name changed

--- resources/views/points/index.blade.php
+++ resources/views/points/index.blade.php
@@ -2,21 +2,21 @@
 
 @section('content')
     <div class="container py-12">
-        <h1>Mijn Festivalmuntjes</h1>
+        <h1>Mijn FestivalPunten</h1>
         
         <div class="card my-4 p-4 shadow">
-            <h2>Totaal Aantal Muntjes</h2>
-            <p>Je hebt {{ $points->amount ?? 0 }} muntjes</p>
+            <h2>Totaal Aantal Punten</h2>
+            <p>Je hebt {{ $points->amount ?? 0 }} Punten</p>
         </div>
         
         <div class="card my-4 p-4 shadow">
-            <h2>Hoe Ik Muntjes Heb Verdiend</h2>
+            <h2>Hoe Ik Punten Heb Verdiend</h2>
             @if($bookings->isEmpty())
-                <p>Je hebt nog geen busreizen geboekt en daarmee nog geen muntjes verdiend.</p>
+                <p>Je hebt nog geen busreizen geboekt en daarmee nog geen punten verdiend.</p>
             @else
                 <ul>
                     @foreach($bookings as $booking)
-                        <li>{{ $booking->bus->festival->naam }} - {{ $booking->created_at->format('d-m-Y') }} - +5 muntjes</li>
+                        <li>{{ $booking->bus->festival->naam }} - {{ $booking->created_at->format('d-m-Y') }} - +5 Punten</li>
                     @endforeach
                 </ul>
             @endif
